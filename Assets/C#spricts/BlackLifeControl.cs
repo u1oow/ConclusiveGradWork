9fd4cb95c7953836c057e0317da1839dd457409f
u1oow <141417522+u1oow@users.noreply.github.com>
2023-08-26 19:53:36 +0900

    showupHPIconのプログラムについて

--- Assets/C#spricts/BlackLifeControl.cs
+++ Assets/C#spricts/BlackLifeControl.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-public class LifeControl : MonoBehaviour
+
+public class BlackLifeControl : MonoBehaviour
 {
     [SerializeField, Header("HPアイコン")]
     #region//プライベート変数
@@ -30,49 +30,12 @@ public class LifeControl : MonoBehaviour
     /// <returns>ライフ接置</returns>
     private void CreateHPIcon()
     {
-        for(int i = 0;i < player.GetHP(); i++)
+        for (int i = 0; i < player.GetHP(); i++)
         {
             GameObject playerHPObj = Instantiate(playerIcon);
             playerHPObj.transform.parent = transform;
             //生成したHPアイコンの親オブジェクトにプレイヤーHPが入っているゲームオブジェクトを指定します
             //この時点でハートのクローンの複製をすることが可能
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-       ShowHPIcon();
-        
-        /*/
-        if(life)
-        {
-            //anim.SetBool("LifeOn",true );
-        }
-        else
-        {
-            //anim.SetBool("LifeOn",false);
-        }
-        /*/
-    }
-
-    /// <summary>
-    /// プレイヤーHPに応じたライフを表示する
-    /// </summary>
-    /// <returns>ライフ管理</returns>
-    
-    private void ShowHPIcon()
-    {
-        if (beforeHP == player.GetHP()) return;
-
-        Image[] icons = transform.GetComponentsInChildren<Image>();
-        for(int i =0;i < icons.Length; i++)
-        {
-            icons[i].gameObject.SetActive(i < player.GetHP());
-            //life = false;
-
-            //iの番号に応じて、順番にハートを出してくる
-        }
-        beforeHP = player.GetHP();
     }
 }
